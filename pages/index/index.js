245192cc3844d3474f52f0ac7d0628a5fba0124f
AllenChinese <13402202685@163.com>
2018-10-12 10:44:29 +0800

    test: 添加一张轮播图。

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,7 @@ Page({
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
     indicatorDots: false,
