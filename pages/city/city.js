2f2fb9edbb19a710ffa50b2344be601287f77258
AllenChinese <13402202685@163.com>
2019-01-06 00:08:09 +0800

    feat: 添加城市选择页面

--- pages/city/city.js
+++ pages/city/city.js
@@ -1,20 +1,11 @@
-// pages/me/me.js
+// pages/city/city.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    imgUrls: [
-      '../../assets/images/banner/banner1.jpeg',
-      '../../assets/images/banner/banner2.jpeg',
-      '../../assets/images/banner/banner3.jpeg',
-      '../../assets/images/banner/banner4.jpeg'
-    ],
-    indicatorDots: false,
-    autoplay: true,
-    interval: 3000,
-    duration: 1000
+
   },
 
   /**
@@ -71,14 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-
-  /**
-   * 调转到城市选择
-   */
-  pageToCity: function () {
-    wx.navigateTo({
-      url: '/pages/city/city'
-    })
   }
 })
