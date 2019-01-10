d6d67106a9c5b1625dfeb80c8ed7acc5f42e651e
AllenChinese <13402202685@163.com>
2019-01-10 23:34:39 +0800

    feat: 「我的活动」添加循环逻辑

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -6,8 +6,7 @@ Page({
    */
   data: {
     imgUrls: [
-      '../../assets/images/banner/banner1.jpeg',
-      '../../assets/images/banner/banner2.jpeg',
+
       '../../assets/images/banner/banner3.jpeg',
       '../../assets/images/banner/banner4.jpeg'
     ],
@@ -15,13 +14,59 @@ Page({
     autoplay: false,
     interval: 3000,
     duration: 1000,
-    searchValue: ''
+    searchValue: '',
+    activitiesData: [
+      {
+        previewUrl: '../../assets/images/banner/banner1.jpeg',
+        title: '深度工作：如何有效使用每一点脑力',
+        sponsor: 'Allen周嘉炜',
+        address: '浙江省杭州市西湖区湖滨银泰49号海底捞',
+        date: '2019.1.10',
+        person: [
+          {
+            imgUrl: '',
+            name: 'Allen周嘉炜',
+            id: ''
+          },
+          {
+            imgUrl: '',
+            name: '赵海豪',
+            id: ''
+          },
+          {
+            imgUrl: '',
+            name: '二爷',
+            id: ''
+          },
+          {
+            imgUrl: '',
+            name: '二爷',
+            id: ''
+          }
+        ]
+      },
+      {
+        previewUrl: '../../assets/images/banner/banner2.jpeg',
+        title: '努力工作：如何有效使用每一点脑力',
+        sponsor: 'Allen周嘉炜',
+        address: '浙江省杭州市西湖区湖滨银泰49号海底捞',
+        date: '2019.1.10',
+        person: [
+          {
+            imgUrl: '',
+            name: 'Allen周嘉炜',
+            id: ''
+          }
+        ]
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.initActivityPerson()
 
   },
 
@@ -72,5 +117,29 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 生成 activity-person 文案
+   */
+  initActivityPerson: function () {
+    let that = this
+    this.data.activitiesData.forEach((ele, index) => {
+      if (ele.person.length <= 1) {
+        ele.personWord = '还没有好友参与呢，去分享你的活动吧'
+      } else {
+        let personWord = ''
+        ele.person.forEach((_ele, _index) => {
+          if (_index < 2) {
+            personWord += _ele.name + '、'
+          }
+        })
+        ele.personWord = personWord.substring(0, personWord.length - 1) + '等' + ele.person.length + '位好友参与中'
+      }
+      that.setData({
+        activitiesData: that.data.activitiesData
+      })
+    })
+    console.log(this.data.activitiesData)
   }
 })
