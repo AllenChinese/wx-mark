1e77d8649328ee509d45d54ac494de89afab8b49
AllenChinese <13402202685@163.com>
2019-01-11 23:49:56 +0800

    feat: 「活动搜索」新增快速入口 & 为你推荐

---
+++ components/c-recommend/c-recommend.js
@@ -0,0 +1,27 @@
+Component({
+  properties: {
+    // 这里定义了innerText属性，属性值可以在组件使用时指定
+    innerText: {
+      type: String,
+      value: 'default value',
+    }
+  },
+  data: {
+    // 这里是一些组件内部数据
+    isLoading: false
+  },
+  methods: {
+    onSearch() { },
+    onCancel() { },
+    changeBatchEvent: function () {
+      if (this.data.isLoading) {
+        return false
+      }
+      // api callback success
+      this.setData({
+        isLoading: !this.data.isLoading
+      })
+      console.log(this.data.isLoading)
+    }
+  }
+})
--- pages/activityDetail/activityDetail.js
+++ pages/activityDetail/activityDetail.js
@@ -1,65 +1,11 @@
-// pages/activity/activity.js
+// pages/activityDetail/activityDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // imgUrls: [
 
-    //   '../../assets/images/banner/banner3.jpeg',
-    //   '../../assets/images/banner/banner4.jpeg'
-    // ],
-    // indicatorDots: false,
-    // autoplay: false,
-    // interval: 3000,
-    // duration: 1000,
-    // searchValue: '',
-    // activitiesData: [
-    //   {
-    //     previewUrl: '../../assets/images/banner/banner1.jpeg',
-    //     title: '深度工作：如何有效使用每一点脑力',
-    //     sponsor: 'Allen周嘉炜',
-    //     address: '浙江省杭州市西湖区湖滨银泰49号海底捞',
-    //     date: '2019.1.10',
-    //     person: [
-    //       {
-    //         imgUrl: '',
-    //         name: 'Allen周嘉炜',
-    //         id: ''
-    //       },
-    //       {
-    //         imgUrl: '',
-    //         name: '赵海豪',
-    //         id: ''
-    //       },
-    //       {
-    //         imgUrl: '',
-    //         name: '二爷',
-    //         id: ''
-    //       },
-    //       {
-    //         imgUrl: '',
-    //         name: '二爷',
-    //         id: ''
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     previewUrl: '../../assets/images/banner/banner2.jpeg',
-    //     title: '努力工作：如何有效使用每一点脑力',
-    //     sponsor: 'Allen周嘉炜',
-    //     address: '浙江省杭州市西湖区湖滨银泰49号海底捞',
-    //     date: '2019.1.10',
-    //     person: [
-    //       {
-    //         imgUrl: '',
-    //         name: 'Allen周嘉炜',
-    //         id: ''
-    //       }
-    //     ]
-    //   }
-    // ]
   },
 
   /**
