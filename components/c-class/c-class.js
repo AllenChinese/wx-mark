ec39f243cfc44bfe4c7a666793ac1e1e9e640ecf
AllenChinese <13402202685@163.com>
2019-01-13 01:18:59 +0800

    feat:  新增「分类」页

--- components/c-class/c-class.js
+++ components/c-class/c-class.js
@@ -2,9 +2,12 @@ Component({
   data: {
 
   },
+  externalClasses: ['external-class-part', 'external-class-title', 'external-class-btn'],
   methods: {
     goToClassDetailPage: function () {
-
+      wx.navigateTo({
+        url: '/pages/allClass/allClass'
+      })
     }
   },
 
