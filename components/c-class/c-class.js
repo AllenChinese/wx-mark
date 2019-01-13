3b8f220c5c58569c7e12b3b5189ff009e103e8d2
AllenChinese <13402202685@163.com>
2019-01-13 14:37:18 +0800

    feat(module): 新增分类列表页面

--- components/c-class/c-class.js
+++ components/c-class/c-class.js
@@ -4,9 +4,20 @@ Component({
   },
   externalClasses: ['external-class-part', 'external-class-title', 'external-class-btn'],
   methods: {
+    /**
+     * 跳转到全部分类页面
+     */
     goToClassDetailPage: function () {
       wx.navigateTo({
         url: '/pages/allClass/allClass'
+      })
+    },
+    /**
+     * 跳转到对应分类下的列表
+     */
+    goToActivityList: function () {
+      wx.navigateTo({
+        url: "/pages/activityList/activityList"
       })
     }
   },
