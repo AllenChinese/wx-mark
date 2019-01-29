9af7088152244f447ee9d1f16fdabe84473b8aa8
AllenChinese <13402202685@163.com>
2019-01-30 00:19:08 +0800

    feat: 「活动详情页」发布

--- pages/activityDetail/activityDetail.js
+++ pages/activityDetail/activityDetail.js
@@ -5,6 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
+    activity: {
+      preview: '../../assets/images/banner/banner1.jpeg',
+      title: '网购食品热量红黑榜',
+      collection: 930,
+      read: 1090,
+      desc: '在火锅都能网购的时代，你购物车里的好吃的，哪些是减脂增肌小帮手，那些是热量炸弹？',
+      time: '2018/12/01 12:00 - 2018/12/02 18:00',
+      location: '杭州市萧山区宁围镇春和钱塘'
+    }
 
   },
 
