ea47c2c325e89e42bd8aac82e5554446cb755346
AllenChinese <13402202685@163.com>
2019-02-10 22:40:50 +0800

    chore: 按钮文字颜色统一处理

--- components/c-hot/c-hot.js
+++ components/c-hot/c-hot.js
@@ -6,10 +6,20 @@ Component({
     }
   },
   data: {
-
+    // 这里是一些组件内部数据
+    isLoading: false
   },
 
   methods: {
-
+    changeBatchEvent: function () {
+      if (this.data.isLoading) {
+        return false
+      }
+      // api callback success
+      this.setData({
+        isLoading: !this.data.isLoading
+      })
+      console.log(this.data.isLoading)
+    }
   }
 })
