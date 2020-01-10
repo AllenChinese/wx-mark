1c38a17f775820e806fd9ff6c52de01806118622
AllenChinese <13402202685@163.com>
2020-01-10 14:29:41 +0800

    feat: 问卷调查表单提交

--- http/apis/me.js
+++ http/apis/me.js
@@ -1,1 +1,12 @@
 import wxRequest from '../wxRequest'
+
+/**
+ * 问卷调查提交
+ */
+export const questionnaireSubmit = (payload) => {
+  return wxRequest({
+    method: 'post',
+    url: 'v1/questionnaire/submit',
+    data: payload,
+  })
+}
--- pages/me-questionnaire/me-questionnaire.js
+++ pages/me-questionnaire/me-questionnaire.js
@@ -1,7 +1,8 @@
-import Notify from '../../miniprogram_npm/vant-weapp/notify/notify';
+import Notify from '../../miniprogram_npm/vant-weapp/notify/notify'
+import regeneratorRuntime from '../../libs/regenerator-runtime/runtime-module'
+import { questionnaireSubmit } from '../../http/apis/me'
 // pages/me-q/me-q.js
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -9,92 +10,76 @@ Page({
     time: null,
     isLoading: false,
     formdata: {
-      render: '1',
-      age: '<18',
-      where: '好友圈',
-      frequency: '至少每周一次',
-      newFun: '',
-      mind: ''
-    }
+      gender: '2',
+      age: '<=18',
+      wayForGetMsg: '好友圈',
+      frequencyOfParticipation: '至少每周一次',
+      mostWantedFeature: '',
+      suggest: '',
+    },
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  },
+  onShareAppMessage: function() {},
 
   /**
    * 单选框形式的触发事件
    */
-  onClick: function (event) {
+  onClick: function(event) {
     this.data.formdata[event.currentTarget.dataset.key] = event.currentTarget.dataset.name
     this.setData({
-      formdata: this.data.formdata
+      formdata: this.data.formdata,
     })
   },
 
   /**
    * fild 输入框形式的触发事件
    */
-  onChange: function (event) {
+  onChange: function(event) {
     clearTimeout(this.data.time)
     const that = this
 
     that.data.time = setTimeout(() => {
       that.data.formdata[event.currentTarget.dataset.key] = event.detail
       that.setData({
-        formdata: that.data.formdata
+        formdata: that.data.formdata,
       })
     }, 300)
   },
@@ -102,24 +87,36 @@ Page({
   /**
    * 表单提交
    */
-  submitEvent: function () {
-
-    if (!this.data.isLoading) { // 保证非正在提交中
+  submitEvent: async function() {
+    if (!this.data.isLoading) {
+      // 保证非正在提交中
       this.setData({
-        isLoading: true
+        isLoading: true,
       })
-      setTimeout(() => {
+      let res = await questionnaireSubmit({
+        ...this.data.formdata,
+      })
+      if (res.errorCode === 0) {
         this.setData({
-          isLoading: false
+          isLoading: false,
+          formdata: {
+            gender: '2',
+            age: '<=18',
+            wayForGetMsg: '好友圈',
+            frequencyOfParticipation: '至少每周一次',
+            mostWantedFeature: '',
+            suggest: '',
+          },
         })
         Notify({
           text: '提交成功，感谢您的参与',
           duration: 1000,
           selector: '#custom-selector',
-          backgroundColor: '#a061ff'
+          backgroundColor: '#a061ff',
         })
-      }, 2000)
+      } else {
+        console.log(res.msg)
+      }
     }
-
-  }
+  },
 })
